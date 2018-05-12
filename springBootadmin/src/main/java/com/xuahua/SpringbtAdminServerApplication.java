1ba01cf268c0b06c11ff75ef1ca53a9fbf540778
publicwyong <publicwyong@126.com>
2018-05-12 22:36:03 +0800

    新增加springbootadmin

---
+++ springBootadmin/src/main/java/com/xuahua/SpringbtAdminServerApplication.java
@@ -0,0 +1,25 @@
+package com.xuahua;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wyong
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}
+ * @date 2018/5/1022:29
+ */
+@EnableAdminServer
+//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+public class SpringbtAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbtAdminServerApplication.class, args);
+    }
+}
