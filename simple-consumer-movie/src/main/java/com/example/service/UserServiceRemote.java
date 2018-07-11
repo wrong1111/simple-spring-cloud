98f3034a70efe4f843c15a7892155c2f1c129272
publicwyong <publicwyong@126.com>
2018-07-11 12:52:48 +0800

    provider user方法请求改成了 /user?id=xx 所以此处robion请求服务要做调整

--- simple-consumer-movie/src/main/java/com/example/service/UserServiceRemote.java
+++ simple-consumer-movie/src/main/java/com/example/service/UserServiceRemote.java
@@ -3,6 +3,7 @@ package  com.example.service;
 
 import com.example.pojo.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name= "SIMPLE-PROVIDER-USER")
 public interface UserServiceRemote {
 
-    @RequestMapping(value = "/user")
+    @RequestMapping(value = "/user/find")
     public User findUserById(@RequestParam(value = "id") Integer id);
+
+
+    @RequestMapping(value = "/user/find/{id}")
+    public User findById(@PathVariable Integer id);
 }
