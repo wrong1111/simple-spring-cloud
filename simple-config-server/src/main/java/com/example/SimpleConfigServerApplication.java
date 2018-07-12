971b25b25eadc0f98bb8f50f1c03274d69163f97
publicwyong <publicwyong@126.com>
2018-07-12 15:55:00 +0800

    add config directory

---
+++ simple-config-server/src/main/java/com/example/SimpleConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.simpleconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleConfigServerApplication.class, args);
+	}
+}
