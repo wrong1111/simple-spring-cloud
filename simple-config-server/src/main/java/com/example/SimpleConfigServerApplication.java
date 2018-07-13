74c77d0148d738eab72f534ba83c12c075fc004e
publicwyong <publicwyong@126.com>
2018-07-13 11:04:01 +0800

    注册配置中心，让发现服务发现。

--- simple-config-server/src/main/java/com/example/SimpleConfigServerApplication.java
+++ simple-config-server/src/main/java/com/example/SimpleConfigServerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 让注册中心发现配置服务中心
+ * */
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
