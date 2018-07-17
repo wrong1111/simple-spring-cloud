ff929c66e5563f43b1a0dd23d128866f71201eb3
publicwyong <publicwyong@126.com>
2018-07-17 16:22:37 +0800

    zipkin 采用rabbitmq

--- simple-zipkin-server/src/main/java/com/example/SimpleZipkinServerApplication.java
+++ simple-zipkin-server/src/main/java/com/example/SimpleZipkinServerApplication.java
@@ -3,11 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZipkinServer
+//@EnableZipkinServer
+@EnableZipkinStreamServer
 public class SimpleZipkinServerApplication {
 
 	public static void main(String[] args) {
